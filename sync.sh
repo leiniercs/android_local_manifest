15d26ef0fc7fbfde0589796bb010503476ee8492
Leinier Cruz Salfran <leiniercs@gmail.com>
2022-07-27 21:25:13 +0000

    Fixes

--- sync.sh
+++ sync.sh
@@ -12,14 +12,15 @@ if [ ! -d $ROM_DIR ]; then
   mkdir -p $ROM_DIR
 fi
 cd $ROM_DIR
+ls -1
 
 # Initialize the repository
-if [ ! -e .repo_initiated ]; then
-  repo init --manifest-url=https://github.com/$ROM/manifest --manifest-branch=$FLAVOR --groups=default,-darwin,-mips,-notdefault
-  mkdir .repo/local_manifests
-  touch .repo_initiated
+if [ ! -e repo_initiated ]; then
+#  repo init --manifest-url=https://github.com/$ROM/manifest --manifest-branch=$FLAVOR --groups=default,-darwin,-mips,-notdefault
+  mkdir -p .repo/local_manifests
+  touch repo_initiated
 fi
 cp ../../../../local_manifest.xml .repo/local_manifests/
 
 # Sync the repository
-repo sync --jobs=8 --current-branch --no-clone-bundle --optimized-fetch
+#repo sync --jobs=8 --current-branch --no-clone-bundle --optimized-fetch
