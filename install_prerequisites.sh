9b25f1370ac1aeba89cf720a4ec6e72e5798062c
Leinier Cruz Salfran <leiniercs@gmail.com>
2022-07-27 01:35:57 +0000

    Fixes

--- install_prerequisites.sh
+++ install_prerequisites.sh
@@ -1,34 +1,36 @@
 export NCPU=$(nproc --all)
 
 # Updating sources package listings
-#cat << EOF > /etc/apt/sources.list
-#deb http://archive.ubuntu.com/ubuntu jammy main restricted universe multiverse
-#deb http://archive.ubuntu.com/ubuntu jammy-updates main restricted universe multiverse
-#deb http://archive.ubuntu.com/ubuntu jammy-backports main restricted universe multiverse
-#deb http://security.ubuntu.com/ubuntu jammy-security main restricted universe multiverse
-#EOF
-#apt -q -y update
+cat << EOF > /etc/apt/sources.list
+deb http://archive.ubuntu.com/ubuntu jammy main restricted universe multiverse
+deb http://archive.ubuntu.com/ubuntu jammy-updates main restricted universe multiverse
+deb http://archive.ubuntu.com/ubuntu jammy-backports main restricted universe multiverse
+deb http://security.ubuntu.com/ubuntu jammy-security main restricted universe multiverse
+EOF
+apt -q -y update
 
 # Upgrading the system
-#apt -q -y dist-upgrade
+apt -q -y dist-upgrade
 
 # Installing packages
-#apt -q -y install lsb-core autoconf automake axel bison \
-#               ccache clang cmake ninja-build soong expat flex \
-#               g++-multilib gawk gcc-multilib gnupg gperf \
-#               imagemagick lib32ncurses5-dev lib32z1-dev libtinfo5 libcap-dev \
-#               libexpat1-dev libmpc-dev libmpfr-dev libncurses5-dev \
-#               libprotobuf-dev libnl-route-3-dev libtool protobuf-compiler \
-#               libsdl1.2-dev libssl-dev libtool libxml2-utils '^lzma.*' lzop \
-#               maven ncftp ncurses-dev patchelf pkg-config pngcrush \
-#               pngquant python2.7 python-all-dev re2c schedtool squashfs-tools subversion \
-#               texinfo w3m xsltproc zlib1g-dev lzip \
-#               libxml-simple-perl libswitch-perl libncurses5 python-is-python3 \
-#               pinentry-curses gpg-agent
+apt -q -y install lsb-core autoconf automake axel bison \
+               ccache clang cmake ninja-build soong expat flex \
+               g++-multilib gawk gcc-multilib gnupg gperf \
+               imagemagick lib32ncurses5-dev lib32z1-dev libtinfo5 libcap-dev \
+               libexpat1-dev libmpc-dev libmpfr-dev libncurses5-dev \
+               libprotobuf-dev libnl-route-3-dev libtool protobuf-compiler \
+               libsdl1.2-dev libssl-dev libtool libxml2-utils '^lzma.*' lzop \
+               maven ncftp ncurses-dev patchelf pkg-config pngcrush \
+               pngquant python2.7 python-all-dev re2c schedtool squashfs-tools subversion \
+               texinfo w3m xsltproc zlib1g-dev lzip \
+               libxml-simple-perl libswitch-perl libncurses5 python-is-python3 \
+               pinentry-curses gpg-agent
 
 # Installing Android Repository Manager
-curl https://storage.googleapis.com/git-repo-downloads/repo > /usr/bin/repo
-chmod a+x /usr/bin/repo
+mkdir ~/bin
+curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
+chmod a+x ~/bin/repo
+export PATH=$PATH:~/bin
 
 # Preparing Git
 git config --global user.name "$GIT_NAME"
