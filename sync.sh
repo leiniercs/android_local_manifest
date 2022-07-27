dbec89402dfdad3414ee45d10555b894cec1b24f
Leinier Cruz Salfran <leiniercs@gmail.com>
2022-07-27 02:18:09 +0000

    Fixes

--- sync.sh
+++ sync.sh
@@ -6,7 +6,7 @@ mkdir -p $DEVICE/$ROM
 cd $DEVICE/$ROM
 
 # Initialize the repository
-repo init --depth=1 --manifest-url=https://github.com/PixelExperience/manifest --branch=twelve-plus --groups=default,-device,-mips,-darwin,-notdefault
+repo init --manifest-url=https://github.com/PixelExperience/manifest --manifest-branch=twelve-plus --groups=default,-device,-mips,-darwin,-notdefault --depth=1
 mkdir .repo/local_manifests
 mv ~/local_manifest.xml .repo/local_manifests/
 
