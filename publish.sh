faa6e4c9c2b52fd1d131596085b887c1012bd478
Leinier Cruz Salfran <leiniercs@gmail.com>
2022-07-27 16:43:31 +0000

    Fixes

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 # Publish
-ROM_FILE=$(basename $ROM_DIR/out/target/product/$DEVICE/*.zip)
-curl --upload-file $ROM_DIR/out/target/product/$DEVICE/$ROM_FILE https://transfer.sh/$ROM_FILE
+ROM_FILE=$(basename out/target/product/$DEVICE/*.zip)
+curl --upload-file out/target/product/$DEVICE/$ROM_FILE https://transfer.sh/$ROM_FILE
 
 # Clean
-rm -fr $ROM_DIR/out
+rm -fr out
