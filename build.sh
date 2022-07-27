0ca2bd958e5c0e6c5aa5bf84caba569d7e1a8256
Leinier Cruz Salfran <leiniercs@gmail.com>
2022-07-27 18:17:23 +0000

    Fixes

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 export NCPU=$(nproc --all)
+export ROM_DIR="roms/$DEVICE/$ROM/$FLAVOR"
 
 # Build
+cd $ROM_DIR
 source build/envsetup.sh
 lunch aosp_$DEVICE-user
 mka bacon -j$NCPU
--- publish.sh
+++ publish.sh
@@ -1,4 +1,7 @@
+export ROM_DIR="roms/$DEVICE/$ROM/$FLAVOR"
+
 # Publish
+cd $ROM_DIR
 ROM_FILE=$(basename out/target/product/$DEVICE/*.zip)
 curl --upload-file out/target/product/$DEVICE/$ROM_FILE https://transfer.sh/$ROM_FILE
 
