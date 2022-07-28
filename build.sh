d83cccb108e037754e1568d565012fc9ee23de81
Leinier Cruz Salfran <leiniercs@gmail.com>
2022-07-28 22:24:32 +0000

    Fixes

--- build.sh
+++ build.sh
@@ -4,8 +4,9 @@ export ROM_DIR="roms/$DEVICE/$ROM/$FLAVOR"
 # Build
 cd $ROM_DIR
 source build/envsetup.sh
-lunch aosp_$DEVICE-user
-$BUILD_COMMAND -j$NCPU
+#lunch aosp_$DEVICE-user
+#$BUILD_COMMAND -j$NCPU
+brunch garden
 
 # Terminate GPG Agent daemon
 #pkill gpg-agent
