0f09aae666c10def2beb616b3874841faa7ce9e1
Leinier Cruz Salfran <leiniercs@gmail.com>
2022-07-29 21:09:38 +0000

    garden-crdroidandroid-12.1

--- sync.sh
+++ sync.sh
@@ -15,8 +15,8 @@ sudo service nfs-common start
 #sudo wg-quick up wg0
 #sudo apt -y install cifs-utils
 #sudo mount -v -o uid=1000,gid=1000 -t cifs //100.64.0.1/aosp $CURDIR/roms
-sudo mount -o port=9402 168.235.81.234:/srv/aosp $CURDIR/roms
-#sudo mount -o vers=3 [2604:180:f3::421]:/srv/aosp $CURDIR/roms
+sudo mount -o nfsvers=3,port=9402 168.235.81.234:/srv/aosp $CURDIR/roms
+#sudo mount -o nfsvers=3,port=9402 [2604:180:f3::421]:/srv/aosp $CURDIR/roms
 
 # Setting up Git
 git config --global user.name "$GIT_NAME"
