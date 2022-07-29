b388dd1059a309f48b14b211187afab9a3df0e53
Leinier Cruz Salfran <leiniercs@gmail.com>
2022-07-29 03:10:47 +0000

    Fixes

--- sync.sh
+++ sync.sh
@@ -3,8 +3,6 @@ export ROM_DIR="roms/$DEVICE/$ROM/$FLAVOR"
 
 # Preparing the remote folder
 mkdir roms
-CURDIR=$(pwd)
-sudo mount -v -t cifs //[2604:180:f3::421]/aosp $CURDIR/roms
 
 # Setting up Git
 git config --global user.name "$GIT_NAME"
