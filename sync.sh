1131cc8a72994788a29adaf977b18f04bb427053
Leinier Cruz Salfran <leiniercs@gmail.com>
2022-07-31 15:34:00 +0000

    crDroid-garden-12.1

--- sync.sh
+++ sync.sh
@@ -10,12 +10,12 @@ mv local_manifest.xml ~/
 cd ~
 mkdir roms
 CURDIR=$(pwd)
-#sudo service rpcbind start
-#sudo service nfs-common start
+sudo service rpcbind start
+sudo service nfs-common start
 #sudo wg-quick up wg0
 #sudo apt -y install cifs-utils
 #sudo mount -v -o uid=1000,gid=1000 -t cifs //100.64.0.1/aosp $CURDIR/roms
-sudo mount -o udp,port=9402 168.235.81.234:/ $CURDIR/roms
+sudo mount -t nfs -o port=9402,sec=sys 168.235.81.234:/ $CURDIR/roms
 #sudo mount -o udp,port=9402 [2604:180:f3::421]:/srv/aosp $CURDIR/roms
 
 # Setting up Git
