11c630cd14803668f4367c0e338232c77ef5c8a8
Leinier Cruz Salfran <44554001+leiniercs@users.noreply.github.com>
2022-08-02 19:40:57 -0400

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -1,8 +1,8 @@
 export GIT_NAME="Leinier Cruz Salfran"
 export GIT_EMAIL="leiniercs@gmail.com"
-export ROM_NAME="crdroidandroid"
-export ROM_BRANCH="12.1"
-export ROM_MANIFEST="android"
+export ROM_NAME="PixelExperience"
+export ROM_BRANCH="twelve-plus"
+export ROM_MANIFEST="manifest"
 export ROM_DIR="roms/$ROM_NAME/$ROM_BRANCH"
 
 # Preparing the ROM folder
@@ -10,13 +10,13 @@ mv local_manifest.xml ~/
 cd ~
 mkdir roms
 CURDIR=$(pwd)
-sudo service rpcbind start
-sudo service nfs-common start
-sudo wg-quick up wg0
+#sudo service rpcbind start
+#sudo service nfs-common start
+#sudo wg-quick up wg0
 #sudo apt -y install cifs-utils
 #sudo mount -v -o uid=1000,gid=1000 -t cifs //100.64.0.1/aosp $CURDIR/roms
 #sudo mount -v -o port=9402,sec=sys 168.235.81.234:/srv/aosp $CURDIR/roms
-sudo mount -v 100.64.0.1:/ $CURDIR/roms
+#sudo mount -v 100.64.0.1:/ $CURDIR/roms
 #sudo mount -v -t nfs -o port=9402,sec=sys [2604:180:f3::421]:/srv/aosp $CURDIR/roms
 
 # Setting up Git
