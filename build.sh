7d1ca563aad273a4e42795925ab76ce04b626ad3
Leinier Cruz Salfran <44554001+leiniercs@users.noreply.github.com>
2022-08-02 19:43:47 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,20 +2,20 @@ export NCPU=$(nproc --all)
 #export NCPU=64
 export CCACHE_EXEC=$(which ccache)
 export BUILD_HOSTNAME="cirrus-ci.org"
-export DEVICE="garden"
-export ROM_NAME="crdroidandroid"
-export ROM_BRANCH="12.1"
+export DEVICE="dandelion"
+export ROM_NAME="PixelExperience"
+export ROM_BRANCH="twelve-plus"
 export ROM_DIR="roms/$ROM_NAME/$ROM_BRANCH"
 export OUT_DIR="~/out"
 
 # Build
 cd ~/$ROM_DIR
 source build/envsetup.sh
-lunch lineage_$DEVICE-user
+lunch aosp_$DEVICE-userdebug
 #m bacon -j$NCPU
-make -j$NCPU selinux
-make -j$NCPU bootimage
-make -j$NCPU init
+m selinux -j$NCPU
+m bootimage -j$NCPU
+m init -j$NCPU
 
 # Terminate GPG Agent daemon
 #pkill gpg-agent
