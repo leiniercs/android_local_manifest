15f14e805517952453efcca8b957e525c103b324
Leinier Cruz Salfran <44554001+leiniercs@users.noreply.github.com>
2022-08-03 13:07:12 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,8 +6,10 @@ export ROM_DIR="roms/$ROM_NAME/$ROM_BRANCH"
 # Build
 cd ~/$ROM_DIR
 source build/envsetup.sh
-brunch lineage_$DEVICE-userdebug
-#mka bacon -j$NCPU
+lunch lineage_$DEVICE-userdebug
+m selinux -j$NCPU
+m bootimage -j$NCPU
+m init -j$NCPU
 
 # Terminate GPG Agent daemon
 #pkill gpg-agent
