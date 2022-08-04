44c74e94f7e54246a1bd14f9a8ca0c60d639b277
Leinier Cruz Salfran <leiniercs@gmail.com>
2022-08-04 13:22:56 -0400

    Fixes

--- sync.sh
+++ sync.sh
@@ -12,7 +12,7 @@ sudo service nfs-common start
 #sudo mount -v -o uid=1000,gid=1000 -t cifs //100.64.0.1/aosp $CURDIR/roms
 #sudo mount -v -o port=9402,sec=sys 168.235.81.234:/srv/aosp $CURDIR/roms
 #sudo mount -v 100.64.0.1:/ $CURDIR/roms
-sudo mount -v -o proto=udp6,port=9402,ro,nolock [2604:180:f3::421]:/srv/aosp $CURDIR/roms
+sudo mount -v -o proto=udp6,port=9402,ro,nolock [2604:180:f3::421]:/ $CURDIR/roms
 
 # Setting up Git
 git config --global user.name "$GIT_NAME"
