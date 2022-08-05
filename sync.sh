b585edaf8e01cec34bd3344714ecb64276e0578b
Leinier Cruz Salfran <leiniercs@gmail.com>
2022-08-04 23:31:33 -0400

    Fixes

--- sync.sh
+++ sync.sh
@@ -8,7 +8,7 @@ mkdir roms
 #sudo service rpcbind start
 #sudo service nfs-common start
 #sudo mount -v -o uid=1000,gid=1000 -t cifs //100.64.0.1/aosp $CURDIR/roms
-sudo mount -t nfs4 -v -o proto=udp 168.235.81.234:/ $CURDIR/roms
+sudo mount -t nfs4 -v -o ro 168.235.81.234:/ $CURDIR/roms
 #sudo mount -v 100.64.0.1:/ $CURDIR/roms
 #sudo mount -v -t nfs4 [2604:180:f3::421]:/ /srv
 ls -l roms/
