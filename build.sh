9e558a623fc3c6815b6de49a67b5bed165a35bda
Leinier Cruz Salfran <leiniercs@gmail.com>
2022-08-13 23:59:48 -0400

    Fixes

--- build.sh
+++
@@ -1,9 +0,0 @@
-NCPU=$(nproc --all)
-ROM_DIR="roms/$ROM_NAME/$ROM_BRANCH"
-
-# Build
-cd ~/$ROM_DIR
-source build/envsetup.sh
-lunch lineage_$DEVICE-userdebug
-mka bacon -j$NCPU
-
--- publish.sh
+++
@@ -1,6 +0,0 @@
-ROM_DIR="roms/$ROM_NAME/$ROM_BRANCH"
-
-# Publish
-cd ~/$ROM_DIR
-ROM_FILE=$(basename out/target/product/$DEVICE/*.zip)
-curl --upload-file out/target/product/$DEVICE/$ROM_FILE https://transfer.sh/$ROM_FILE
