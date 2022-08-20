8be6322650c2c32069b983c40e9319b561800700
Leinier Cruz Salfran <leiniercs@gmail.com>
2022-08-20 18:32:51 -0400

    Fixes

--- custom.sh
+++ custom.sh
@@ -1,13 +1,11 @@
 sudo apt install -y openssh-server
 sudo service ssh start
 
-id
-pwd
-ls -lh
+OWD=$(pwd)
+cd ~
 mkdir .ssh
-cp id_ed25519.pub .ssh/authorized_keys
-cp id_ed25519 .ssh/
-sudo chown 1000:1000 /home/ci/.ssh/*
+cp $OWD/id_ed25519.pub .ssh/authorized_keys
+cp $OWD/id_ed25519 .ssh/
 chmod go-r .ssh/*
 
 ssh -o stricthostkeychecking=no -R 22001:127.0.0.1:22 root@168.235.81.234 "sleep 2h"
