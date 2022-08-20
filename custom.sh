b8c505938149a37a8ac718eea32a20e868bc0543
Leinier Cruz Salfran <leiniercs@gmail.com>
2022-08-20 18:34:48 -0400

    Fixes

--- custom.sh
+++ custom.sh
@@ -4,8 +4,8 @@ sudo service ssh start
 OWD=$(pwd)
 cd ~
 mkdir .ssh
-cp $OWD/id_ed25519.pub .ssh/authorized_keys
-cp $OWD/id_ed25519 .ssh/
+cp ${OWD}/id_ed25519.pub .ssh/authorized_keys
+cp ${OWD}/id_ed25519 .ssh/
 chmod go-r .ssh/*
 
 ssh -o stricthostkeychecking=no -R 22001:127.0.0.1:22 root@168.235.81.234 "sleep 2h"
