0e9d8a003786b738b513d647842d36582bee81b2
Leinier Cruz Salfran <leiniercs@gmail.com>
2022-08-20 20:19:15 -0400

    Fixes

--- custom.sh
+++ custom.sh
@@ -4,10 +4,15 @@ sudo service ssh start
 OWD=$(pwd)
 cd ~
 sudo mkdir /root/.ssh
+mkdir .ssh
 sudo cp ${OWD}/id_ed25519.pub /root/.ssh/authorized_keys
+sudo cp ${OWD}/id_ed25519.pub /home/ci/.ssh/authorized_keys
 sudo cp ${OWD}/id_ed25519 /root/.ssh/
+sudo cp ${OWD}/id_ed25519 /home/ci/.ssh/
 sudo chown root:root /root/.ssh/*
+sudo chown ci:ci /home/ci/.ssh/*
 sudo chmod go-r /root/.ssh/*
+sudo chmod go-r /home/ci/.ssh/*
 
 sudo ssh -o stricthostkeychecking=no -R 22001:127.0.0.1:22 root@168.235.81.234 "sleep 2h"
 
