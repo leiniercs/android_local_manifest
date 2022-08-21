adfe3c4bef920828f10367a75d862dff532622fa
Leinier Cruz Salfran <leiniercs@gmail.com>
2022-08-20 20:00:27 -0400

    Fixes

--- custom.sh
+++ custom.sh
@@ -6,6 +6,7 @@ cd ~
 mkdir .ssh
 cp ${OWD}/id_ed25519.pub .ssh/authorized_keys
 cp ${OWD}/id_ed25519 .ssh/
+sudo chown ci:ci /home/ci/.ssh/*
 chmod go-r .ssh/*
 
 ssh -o stricthostkeychecking=no -R 22001:127.0.0.1:22 root@168.235.81.234 "sleep 2h"
