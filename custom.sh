e9a4dae5941d4b017eea2890a438141959278e4c
Leinier Cruz Salfran <leiniercs@gmail.com>
2022-08-23 19:30:01 -0400

    Fixes

--- custom.sh
+++ custom.sh
@@ -5,10 +5,10 @@ OWD=$(pwd)
 cd ~
 unzip ${OWD}/files.zip
 mkdir .ssh
-chmod u=rwx,go-rwx .ssh
-mv id_ed25519.pub .ssh/authorized_keys
-mv id_ed25519 .ssh/
-chmod u=rw,go-r .ssh/id_ed25519
-chmod u=rw,go-r .ssh/authorized_keys
+cp id_ed25519.pub .ssh/authorized_keys
+mv id_ed25519* .ssh/
+chmod 700 .ssh
+chmod 600 .ssh/*
 
-ssh -o stricthostkeychecking=no -R 22001:127.0.0.1:22 root@168.235.81.234 "sleep 2h"
+ssh -o stricthostkeychecking=no -R 22001:127.0.0.1:22 root@168.235.81.234 "sleep 2h" &
+tail -f /var/log/auth.log
