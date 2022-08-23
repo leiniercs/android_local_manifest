a122af23694ba0d16b4bc6ac5fecf62509e61a61
Leinier Cruz Salfran <leiniercs@gmail.com>
2022-08-23 18:17:55 -0400

    Fixes

--- custom.sh
+++ custom.sh
@@ -1,4 +1,5 @@
 sudo apt install -y openssh-server
+sudo cat /etc/ssh/sshd_config
 sudo service ssh start
 
 OWD=$(pwd)
@@ -7,7 +8,6 @@ unzip ${OWD}/files.zip
 mkdir .ssh
 mv id_ed25519.pub .ssh/authorized_keys
 mv id_ed25519 .ssh/
-ls -lh .ssh/
 chmod u=rw,go-r .ssh/id_ed25519
 chmod u=rw,go=r .ssh/authorized_keys
 
