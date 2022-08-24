3e326cfb6759592c13ea6db872f8923206c5ca00
Leinier Cruz Salfran <leiniercs@gmail.com>
2022-08-24 16:00:57 -0400

    Fixes

--- prepare.sh
+++ prepare.sh
@@ -1,7 +1,7 @@
 OWD=$(pwd)
 
 sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
-pacman -Syyu --noconfirm --needed base-devel multilib-devel openssh nfs-utils sudo resolvconf wireguard-tools git python repo android-tools android-udev ccache squashfs-tools schedtool bc rsync vim jdk11-openjdk
+pacman -Syyu --noconfirm --needed base-devel multilib-devel openssh nfs-utils sudo resolvconf wireguard-tools git python repo ccache unzip android-tools
 cat /etc/makepkg.conf
 export USE_CCACHE=1
 export CCACHE_EXEC=$(which ccache)
