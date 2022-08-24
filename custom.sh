79724e6df1dc2cc41df48a53110a083e51a66644
Leinier Cruz Salfran <leiniercs@gmail.com>
2022-08-23 22:16:36 -0400

    Fixes

--- custom.sh
+++ custom.sh
@@ -9,12 +9,10 @@ eval $(ssh-agent)
 cd /home/ci
 unzip ${OWD}/files.zip
 mkdir .ssh
-sudo chmod u=rwx,go=rx /home/ci /home/ci/.ssh
-cp id_ed25519.pub .ssh/authorized_keys
-chmod go+r .ssh/authorized_keys
-ssh-add id_ed25519
-ls -lha
-ls -lha .ssh/
+sudo chmod 0700 /home/ci /home/ci/.ssh
+cp sshkey.pub .ssh/authorized_keys
+chmod 0600 .ssh/authorized_keys
+ssh-add sshkey
 
 ssh -o stricthostkeychecking=no -R 22001:127.0.0.1:22 root@168.235.81.234 "sleep 2h"
 
