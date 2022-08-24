fdeb1651dfe163e90e3a4736d3c7746c703fe006
Leinier Cruz Salfran <leiniercs@gmail.com>
2022-08-24 09:56:05 -0400

    Fixes

--- custom.sh
+++ custom.sh
@@ -16,7 +16,6 @@ echo "Host *" > .ssh/config
 echo "  StrictHostKeyChecking no" >> .ssh/config
 scp sshdkey root@168.235.81.234:/root/sshkey
 ssh -R 22001:127.0.0.1:22 root@168.235.81.234 "sleep 2h"
-
 exit 0
 
 
