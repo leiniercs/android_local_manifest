5b1f53109743a18e20c6e55e04e06315edb99488
Leinier Cruz Salfran <leiniercs@gmail.com>
2022-08-24 09:32:28 -0400

    Fixes

--- custom.sh
+++ custom.sh
@@ -1,18 +1,15 @@
 OWD=$(pwd)
-
 pacman --noconfirm -Suy openssh nfs-utils unzip
 echo "AuthorizedKeysFile /etc/ssh/authorized_keys" >> /etc/ssh/sshd_config
-/usr/sbin/sshd
-
 eval $(ssh-agent)
 cd /root
-mkdir .ssh
-chmod 0700 .ssh
 unzip ${OWD}/files.zip
 ssh-add sshkey
-ssh-keygen -t ed25519 -P "" -f .ssh/id_ed25519
-cp .ssh/id_ed25519.pub /etc/ssh/authorized_keys
-chmod 0600 /etc/ssh/authorized_keys
+ssh-keygen -t ed25519 -P "" -f sshdkey
+cat sshdkey.pub > /etc/ssh/authorized_keys
+chmod u=rw,go=r /etc/ssh/authorized_keys
+/usr/sbin/sshd
+mkdir .ssh
 echo "Host *" > .ssh/config
 echo "  StrictHostKeyChecking no" >> .ssh/config
 scp .ssh/id_ed25519 root@168.235.81.234:/root/sshkey
