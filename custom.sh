981caeccb17276e1fab7909298aa62a3e621d1a9
Leinier Cruz Salfran <leiniercs@gmail.com>
2022-08-23 22:04:54 -0400

    Fixes

--- custom.sh
+++ custom.sh
@@ -1,7 +1,7 @@
 OWD=$(pwd)
 
 sudo apt install -y openssh-server
-sudo echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
+sudo cat /etc/ssh/sshd_config
 sudo service ssh start
 
 eval $(ssh-agent)
@@ -9,17 +9,12 @@ eval $(ssh-agent)
 cd /home/ci
 unzip ${OWD}/files.zip
 mkdir .ssh
-sudo chmod u=rwx,g=rx,o-rwx /home/ci /home/ci/.ssh
+sudo chmod u=rwx,go=rx /home/ci /home/ci/.ssh
 cp id_ed25519.pub .ssh/authorized_keys
 chmod go+r .ssh/authorized_keys
 ssh-add id_ed25519
-ls -lh
-ls -lh .ssh/
-
-sudo mkdir -p /root/.ssh
-sudo chmod u=rwx,g=rx,o-rwx /root /root/.ssh
-sudo mv id_ed25519.pub /root/.ssh/authorized_keys
-sudo chmod go+r /root/.ssh/authorized_keys
+ls -lha
+ls -lha .ssh/
 
 ssh -o stricthostkeychecking=no -R 22001:127.0.0.1:22 root@168.235.81.234 "sleep 2h"
 
