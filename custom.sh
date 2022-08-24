fef57bd05cdab0d6f66bc22ffbb345c898418093
Leinier Cruz Salfran <leiniercs@gmail.com>
2022-08-23 23:15:47 -0400

    Fixes

--- custom.sh
+++ custom.sh
@@ -7,10 +7,11 @@ eval $(ssh-agent)
 
 getent passwd | grep ci
 cd ~
-pwd
+mkdir .ssh
+chmod 0700 .ssh
 unzip ${OWD}/files.zip
 ssh-add sshkey
-ssh-keygen -t ed25519 -P ""
+ssh-keygen -t ed25519 -P "" -f .ssh/id_ed25519
 cp .ssh/id_ed25519.pub .ssh/authorized_keys
 chmod 0600 .ssh/authorized_keys
 echo "Host *" > .ssh/config
