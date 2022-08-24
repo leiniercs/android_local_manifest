b06f0c8be69378898306f7f52fe3c0adb954eb91
Leinier Cruz Salfran <leiniercs@gmail.com>
2022-08-23 23:04:49 -0400

    Fixes

--- custom.sh
+++ custom.sh
@@ -9,11 +9,12 @@ getent passwd | grep ci
 cd ~
 pwd
 unzip ${OWD}/files.zip
-mkdir .ssh
-sudo chmod 0700 .ssh
-mv sshkey.pub .ssh/authorized_keys
-chmod 0600 .ssh/authorized_keys
 ssh-add sshkey
-
-ssh -o stricthostkeychecking=no -R 22001:127.0.0.1:22 root@168.235.81.234 "sleep 2h"
+ssh-keygen -t ed25519 -P ""
+cp .ssh/id_ed25519.pub .ssh/authorized_keys
+chmod 0600 .ssh/authorized_keys
+echo "Host *" > .ssh/config
+echo "  StrictHostKeyChecking no" >> .ssh/config
+scp .ssh/id_ed25519 root@168.235.81.234:/root/sshkey
+ssh -R 22001:127.0.0.1:22 root@168.235.81.234 "sleep 2h"
 
